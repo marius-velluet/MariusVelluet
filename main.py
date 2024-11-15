6e3666f0f9ad457271720513833fbf8376f3f2db
marius-velluet <marius.velluet@edu.esiee.fr>
2024-11-15 14:39:07 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import string 
+from unidecode import unidecode 
 #### Fonction secondaire
-
 
 def ispalindrome(p):
 
-    # votre code ici
-    
-    return False
+    p = unidecode(p)
+    p = p.upper()
+    p = p.replace(" ","")
+    p = p.translate(str.maketrans('','',string.punctuation))
+    if len(p) == 1 : return True
+    if p == "" : return True
+    if p[0] == p[-1] : 
+        return ispalindrome(p[1:-1])
+    else :
+        return False
+
 
 #### Fonction principale
 
